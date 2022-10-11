0f4e956521a7000721f672478990be42e80db4ec
Jonathan Sales <51421355+JonathanSaless@users.noreply.github.com>
2022-10-11 12:01:04 -0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,13 +1,16 @@
+from ipaddress import ip_address
 import xmlrpc.client
-import cryptocode as cc
 
+servidor = input("Servidor: ")
 setence = input("Digite o texto a ser criptografado: ")
+
 try:
-    with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
-        encryptedText, password = proxy.encryptorText(setence)
+    with xmlrpc.client.ServerProxy("http://"+servidor+":8000/") as proxy:
+        encryptedText = proxy.encryptorText(setence)
         print("Texto criptografado: {}" .format(encryptedText))
-        print(password)
-        decryptedText = cc.decrypt(encryptedText, password)
+
+        decryptedText = proxy.decryptorText(encryptedText)
         print("Texto descriptografado: {}" .format(decryptedText))
+
 except Exception as e:
     print(e)
--- server.py
+++ server.py
@@ -6,12 +6,23 @@ server = SimpleXMLRPCServer(("localhost", 8000))
 print("Listening on port 8000...")
 
 def encryptorText(value):
+    print("----------------------------------------------")
     print("Requisição recebida com o seguinte argumento: " + str(value))
     textEncrypted = cc.encrypt(value, senha)
     print("Texto criptografado: {}" .format(textEncrypted))
-    return textEncrypted, senha
+    print("----------------------------------------------")
+    return textEncrypted
+
+def decryptorText(value):
+    print("----------------------------------------------")
+    print("Requisição recebida com o seguinte argumento: " + str(value))
+    textDecrypted = cc.decrypt(value, senha)
+    print("Texto descriptografado: {}" .format(textDecrypted))
+    print("----------------------------------------------")
+    return textDecrypted
 
 server.register_function(encryptorText, "encryptorText")
+server.register_function(decryptorText, "decryptorText")
 
 try:
     print("Control-C to exit server")
