869e666924ec157b34d85f6a60524fca027bccfc
Jonathan Sales <51421355+JonathanSaless@users.noreply.github.com>
2022-10-11 11:46:39 -0300

    Add files via upload

--- client.py
+++ client.py
@@ -2,10 +2,12 @@ import xmlrpc.client
 import cryptocode as cc
 
 setence = input("Digite o texto a ser criptografado: ")
-
-with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
-    encryptedText, password = proxy.encryptorText(setence)
-    print("Texto criptografado: {}" .format(encryptedText))
-    print(password)
-    decryptedText = cc.decrypt(encryptedText, password)
-    print("Texto descriptografado: {}" .format(decryptedText))
+try:
+    with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
+        encryptedText, password = proxy.encryptorText(setence)
+        print("Texto criptografado: {}" .format(encryptedText))
+        print(password)
+        decryptedText = cc.decrypt(encryptedText, password)
+        print("Texto descriptografado: {}" .format(decryptedText))
+except Exception as e:
+    print(e)
